9c11f6f1f5546cf069956ba86a85a36a49c9191b
V <2486145@cognizant.com>
2026-03-25 17:19:05 +0530

    Initial commit: Hospital Management System Parent Structure

--- hospital-management-system/src/test/java/com/hospital/management/HospitalManagementSystemApplicationTests.java
+++ hospital-management-system/src/test/java/com/hospital/management/HospitalManagementSystemApplicationTests.java
@@ -1,10 +1,10 @@
-package com.hospitalmanagement.patientprofileservice;
+package com.hospital.management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PatientprofileserviceApplicationTests {
+class HospitalManagementSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
