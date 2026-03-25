68aa0aebb704ed8df793d0b0f1556c0eb583e865
V <2486145@cognizant.com>
2026-03-25 15:50:20 +0530

    Base Packages are created

---
+++ patientprofileservice/src/test/java/com/hospitalmanagement/patientprofileservice/PatientprofileserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitalmanagement.patientprofileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientprofileserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
