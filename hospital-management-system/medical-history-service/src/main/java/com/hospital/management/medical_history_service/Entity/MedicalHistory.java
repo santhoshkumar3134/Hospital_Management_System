1e8ad24090edd6e5edff637711534b2c1ef4c05a
Chandramouly V <chandramouly64@gmail.com>
2026-03-26 10:37:17 +0530

    created entity and repo

--- hospital-management-system/medical-history-service/src/main/java/com/hospital/management/medical_history_service/Entity/MedicalHistory.java
+++ hospital-management-system/medical-history-service/src/main/java/com/hospital/management/medical_history_service/Entity/MedicalHistory.java
@@ -1,4 +1,24 @@
 package com.hospital.management.medical_history_service.Entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
 public class MedicalHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long medicalHistoryId;
+    private String disease;
+    private String treatment;
+    private LocalDateTime diagnosedAt;
+    private Long patientId;
 }
--- hospital-management-system/medical-history-service/src/main/java/com/hospital/management/medical_history_service/Repository/MedicalHistoryRepository.java
+++ hospital-management-system/medical-history-service/src/main/java/com/hospital/management/medical_history_service/Repository/MedicalHistoryRepository.java
@@ -1,4 +1,9 @@
 package com.hospital.management.medical_history_service.Repository;
 
-public interface MedicalHistoryRepository {
+import com.hospital.management.medical_history_service.Entity.MedicalHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MedicalHistoryRepository extends JpaRepository<MedicalHistory,Long> {
 }
