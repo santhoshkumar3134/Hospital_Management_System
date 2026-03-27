82f435980870ac7786b28f6458b04341f8714b8d
Kiran K G <kirankg571@gmail.com>
2026-03-27 09:05:17 +0530

    feat: integrated doctor service module

---
+++ doctorservice/src/main/java/com/hospital/management/doctorservice/DoctorserviceApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.management.doctorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorserviceApplication.class, args);
+	}
+
+}
